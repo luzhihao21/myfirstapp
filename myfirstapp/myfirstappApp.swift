ff3a46184c328e5e0b36ad43b76f20c0df4f7574
luzhihao <luzhihao21@gmail.com>
2025-12-17 00:56:43 +0900

    Initial Commit

---
+++ myfirstapp/myfirstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myfirstappApp.swift
+//  myfirstapp
+//
+//  Created by Michael on 2025/12/17.
+//
+
+import SwiftUI
+
+@main
+struct myfirstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
